3cfddaa74607a2928c9894ff3ccc267f306c0941
Victor <steemsoftware@gmail.com>
2018-09-20 13:05:42 -0400

    Add main form GUI

--- SteemSoftware/SteemSoftware/MainForm.cs
+++ SteemSoftware/SteemSoftware/MainForm.cs
@@ -21,5 +21,60 @@ namespace SteemSoftware
 			// TODO: Add constructor code after the InitializeComponent() call.
 			//
 		}
+		
+		void NewToolStripMenuItemClick(object sender, EventArgs e)
+		{
+			
+		}
+		
+		void ExitToolStripMenuItemClick(object sender, EventArgs e)
+		{
+			
+		}
+		
+		void DoubleClickLaunchToolStripMenuItemClick(object sender, EventArgs e)
+		{
+			
+		}
+		
+		void LaunchCenteredToolStripMenuItemClick(object sender, EventArgs e)
+		{
+			
+		}
+		
+		void LaunchButtonClick(object sender, EventArgs e)
+		{
+			
+		}
+		
+		void GithubRepositoryToolStripMenuItemClick(object sender, EventArgs e)
+		{
+			
+		}
+		
+		void BusyorgTagToolStripMenuItemClick(object sender, EventArgs e)
+		{
+			
+		}
+		
+		void SteemitcomTagToolStripMenuItemClick(object sender, EventArgs e)
+		{
+			
+		}
+		
+		void RememberWindowlocationToolStripMenuItemClick(object sender, EventArgs e)
+		{
+			
+		}
+		
+		void RememberWindowsizeToolStripMenuItemClick(object sender, EventArgs e)
+		{
+			
+		}
+		
+		void AskOnExitToolStripMenuItemClick(object sender, EventArgs e)
+		{
+			
+		}
 	}
 }
